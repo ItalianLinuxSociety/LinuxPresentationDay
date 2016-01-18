3fecdb47872a8790e3ae6d3c7722a78fdfae5393
Roberto Guido <bob4job@gmail.com>
2016-01-18 16:06:07 +0100

    tracciamento Piwik

--- commons.php
+++ commons.php
@@ -15,14 +15,14 @@ function doHeader() {
 		<link href="css/leaflet.css" rel="stylesheet" />
 		<script src="js/leaflet.js"></script>
 		<script src="js/jquery-2.1.1.min.js"></script>
-		
+
 		<style>
 			.logo {
 				background-image: url(images/people.png);
 				background-position: center;
 				background-repeat: no-repeat;
 			}
-			
+
 			#map {
 				width: 100%;
 				height: 500px;
@@ -49,24 +49,42 @@ function doHeader() {
 					</div>
 				</div>
 			</div>
-	
+
 	<?php
 }
 
 function doFooter() {
 	?>
-	
+
 			<hr/>
-			
+
 			<div class="row">
 				<div class="col-md-12 well text-right">
 					<p>Powered by <a href="http://linux-presentation-day.org/">Linux Presentation Day</a> and <a href="http://www.ils.org/">Italian Linux Society</a></p>
 				</div>
 			</div>
 		</div>
+
+<!-- Piwik -->
+<script type="text/javascript">
+  var _paq = _paq || [];
+  _paq.push(['disableCookies']);
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//pergamena.lugbs.linux.it/";
+    _paq.push(['setTrackerUrl', u+'piwik.php']);
+    _paq.push(['setSiteId', 22]);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.type='text/javascript'; g.async=true; g.defer=true; g.src=u+'piwik.js'; s.parentNode.insertBefore(g,s);
+  })();
+</script>
+<noscript><p><img src="//pergamena.lugbs.linux.it/piwik.php?idsite=22" style="border:0;" alt="" /></p></noscript>
+<!-- End Piwik Code -->
+
 	</body>
 </html>
-	
+
 	<?php
 }
 
