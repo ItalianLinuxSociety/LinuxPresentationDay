663ce60fc694e9a5d811a9c025a1072e2eaa86b8
Roberto Guido <bob4job@gmail.com>
2016-04-07 03:35:56 +0200

    fine fase pubblicazione preliminare

---
+++ faq.php
@@ -0,0 +1,89 @@
+<?php
+
+require_once('commons.php');
+doHeader();
+
+?>			
+
+<div class="row">
+	<div class="col-md-8 col-md-offset-2">
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Cos'è il Linux Presentation Day?</h3>
+				</div>
+		
+				<p>È una manifestazione per la divulgazione di Linux e del software libero, nata nel 2014 e rapidamente estesa in <strong>Germania, Austria e Svizzera</strong>. Nel 2016 sbarca anche in <strong>Francia, Regno Unito, Portogallo, Grecia, e in Italia!</strong> Si svolge due volte all'anno, in aprile ed in ottobre (quest'ultima ricorrenza coincide con il <a href="http://www.linuxday.it/">Linux Day italiano</a>), ed è costituita da una serie di piccole iniziative locali allestite da gruppi cittadini di volontari tutti nella stessa data.</p>
+				<p><a href="http://www.linux-presentation-day.org/">Qui il sito di riferimento</a>.</p>
+			</div>
+		</div>
+	
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>In cosa consiste?</h3>
+				</div>
+		
+				<p>Ogni gruppo può svolgere le attività che più gradisce, benché il metodo raccomandato sia allestire un banchetto informativo in un luogo pubblico e di passaggio come <strong>una via, una piazza o un centro commerciale</strong> presso cui distribuire materiale informativo e far provare Linux a coloro che passano usando uno o più portatili.</p>
+				<p>Se proprio lo si vuole si può organizzare un evento in stile Linux Day, con dei talk ed un Install Party, ma non solo non è necessario ma è, in questa occasione, apertamente e vivamente sconsigliato.</p>
+				<p>In ogni caso l'importante è farlo tutti nello stesso giorno, ovvero <strong>sabato 30 aprile</strong>.</p>
+			</div>
+		</div>
+	
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Chi può aderire?</h3>
+				</div>
+		
+				<p>Chiunque abbia un poco di tempo e voglia per predisporre un banchetto, sia esso una associazione, un gruppo di amici o anche un singolo individuo.</p>
+			</div>
+		</div>
+	
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Possono esserci più Linux Presentation Day nella stessa città?</h3>
+				</div>
+		
+				<p>Si, e anzi è la nostra speranza! Non importa che siano organizzati dallo stesso gruppo o da persone diverse; poiché le risorse richieste per presidiare un banchetto informativo sono alquanto modeste è auspicabile che lo stesso gruppo si divida in più aree della stessa città, per raggiungere contemporaneamente un pubblico più esteso.</p>
+			</div>
+		</div>
+	
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Mi hai convinto, voglio aderire. Come faccio?</h3>
+				</div>
+		
+				<p>Pensa a dove mettere il tuo/vostro banchetto informativo. Se presso un centro commerciale, inizia da subito a spedire una richiesta di permesso alla direzione. Se in una via o in una piazza, devi chiedere una "occupazione di suolo pubblico" al tuo Comune; sicuramente sul sito del Comune stesso trovi maggiori indicazioni sul modulo da compilare e gli orari presso cui portarlo. Solitamente una "occupazione di suolo pubblico" si paga, ma in molti Comuni viene concessa gratuitamente per eventi che godono di patrocinio. Per consigli e dritte su come gestire la parte burocratica, <a href="mailto:info@linuxday.it">contattaci!</a></p>
+				<p>Dopodiché, anche a seconda della location, ti potrebbe servire dell'attrezzatura: un tavolo, tre o quattro sedie, un piccolo gazebo laddove possibile o necessario, e un paio di portatili per le dimostrazioni pratiche.</p>
+				<p>Infine, compila <a href="registrazione.php">il form di registrazione</a> per farci sapere dove intendete allestire il vostro spazio e per permetterci di pubblicare, nei prossimi mesi, un elenco completo delle adesioni.</p>
+				<p>Per ogni evenienza, <a href="images/logo.svg">scarica qui</a> il logo in formato SVG.</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Vorrei aderire, ma sono da solo...</h3>
+				</div>
+		
+				<p>Se vorresti allestire anche tu il Linux Presentation Day nella tua città, ma hai bisogno di supporto, compila ugualmente il <a href="registrazione.php">form di registrazione</a>: spuntando l'apposita casella segnalerai il tuo interessamento, ed i nostri potenti algoritmi ci permetteranno di metterti in contatto con altri potenziali partecipanti nella tua zona per collaborare insieme!</p>
+			</div>
+		</div>
+		
+		<div class="row">
+			<div class="col-md-12">
+				<div class="page-header">
+					<h3>Ho registrato il mio evento, ma vorrei modificare i parametri</h3>
+				</div>
+		
+				<p>Manda una mail a <a href="mailto:info@linuxday.it">info@linuxday.it</a> per chiedere le modifiche che vuoi (o per qualsiasi altra richiesta / dubbio / perplessità in merito all'iniziativa).</p>
+			</div>
+		</div>
+	</div>
+</div>
+			
+<?php doFooter() ?>
+
--- index.php
+++ index.php
@@ -8,72 +8,69 @@ doHeader();
 <div class="row">
 	<div class="col-md-8 col-md-offset-2">
 		<div class="row">
-			<div class="col-md-12">
-				<div class="page-header">
-					<h3>Cos'è il Linux Presentation Day?</h3>
-				</div>
-		
-				<p>È una manifestazione per la divulgazione di Linux e del software libero, nata nel 2014 e rapidamente estesa in <strong>Germania, Austria e Svizzera</strong>. Nel 2016 sbarcherà anche in <strong>Francia, Regno Unito, Portogallo, Grecia e, insieme a voi, in Italia</strong>. Si svolge due volte all'anno, in aprile ed in ottobre (quest'ultima ricorrenza coincide con il <a href="http://www.linuxday.it/">Linux Day italiano</a>), ed è costituita da una serie di iniziative locali allestite da gruppi cittadini di volontari tutti nella stessa data.</p>
+			<div class="col-md-12">		
+				<p>Il Linux Presentation Day è una manifestazione per la divulgazione di Linux e del software libero, nata nel 2014 e rapidamente estesa in <strong>Germania, Austria e Svizzera</strong>. Nel 2016 sbarca anche in <strong>Francia, Regno Unito, Portogallo, Grecia, e in Italia!</strong> Si svolge due volte all'anno, in aprile ed in ottobre (quest'ultima ricorrenza coincide con il <a href="http://www.linuxday.it/">Linux Day italiano</a>), ed è costituita da una serie di piccole iniziative locali allestite da gruppi cittadini di volontari tutti nella stessa data.</p>
 				<p><a href="http://www.linux-presentation-day.org/">Qui il sito di riferimento</a>.</p>
 			</div>
 		</div>
+		
+		<hr />
 	
 		<div class="row">
 			<div class="col-md-12">
-				<div class="page-header">
-					<h3>In cosa consiste?</h3>
-				</div>
-		
-				<p>Ogni gruppo può svolgere le attività che più gradisce, benché il metodo raccomandato sia allestire un banchetto informativo in un luogo pubblico e di passaggio come <strong>una via, una piazza o un centro commerciale</strong> presso cui distribuire materiale informativo e far provare Linux a coloro che passano usando uno o più portatili.</p>
-				<p>Se proprio lo si vuole si può organizzare un evento in stile Linux Day, con dei talk ed un Install Party, ma non solo non è necessario ma è, in questa occasione, apertamente e vivamente sconsigliato.</p>
-				<p>In ogni caso l'importante è farlo tutti nello stesso giorno, ovvero <strong>sabato 30 aprile</strong>.</p>
+				<div id="map"></div>
 			</div>
 		</div>
-	
+		
+		<hr />
+		
 		<div class="row">
 			<div class="col-md-12">
-				<div class="page-header">
-					<h3>Chi può aderire?</h3>
-				</div>
-		
-				<p>Chiunque abbia un poco di tempo e voglia per predisporre un banchetto, sia esso una associazione, un gruppo di amici o anche un singolo individuo.</p>
-			</div>
-		</div>
-	
-		<div class="row">
-			<div class="col-md-12">
-				<div class="page-header">
-					<h3>Possono esserci più Linux Presentation Day nella stessa città?</h3>
-				</div>
-		
-				<p>Si, e anzi è la nostra speranza! Non importa che siano organizzati dallo stesso gruppo o da persone diverse; poiché le risorse richieste per presidiare un banchetto informativo sono alquanto modeste è auspicabile che lo stesso gruppo si divida in più aree della stessa città, per raggiungere contemporaneamente un pubblico più esteso.</p>
-			</div>
-		</div>
-	
-		<div class="row">
-			<div class="col-md-12">
-				<div class="page-header">
-					<h3>Mi hai convinto, voglio aderire. Come faccio?</h3>
-				</div>
-		
-				<p>Pensa a dove mettere il tuo/vostro banchetto informativo. Se presso un centro commerciale, inizia da subito a spedire una richiesta di permesso alla direzione. Se in una via o in una piazza, devi chiedere una "occupazione di suolo pubblico" al tuo Comune; sicuramente sul sito del Comune stesso trovi maggiori indicazioni sul modulo da compilare e gli orari presso cui portarlo. Solitamente una "occupazione di suolo pubblico" si paga, ma in molti Comuni viene concessa gratuitamente per eventi che godono di patrocinio. Per consigli e dritte su come gestire la parte burocratica, <a href="mailto:info@linuxday.it">contattaci!</a></p>
-				<p>Dopodiché, anche a seconda della location, ti potrebbe servire dell'attrezzatura: un tavolo, tre o quattro sedie, un piccolo gazebo laddove possibile o necessario, e un paio di portatili per le dimostrazioni pratiche.</p>
-				<p>Infine, compila <a href="registrazione.php">il form di registrazione</a> per farci sapere dove intendete allestire il vostro spazio e per permetterci di pubblicare, nei prossimi mesi, un elenco completo delle adesioni.</p>
-				<p>Per ogni evenienza, <a href="images/logo.svg">scarica qui</a> il logo in formato SVG.</p>
+				<p>
+					Presso le rispettive locations potrete trovare banchetti informativi e dimostrativi, e maggiori dettagli sulle attività che si svolgono nella tua zona.
+				</p>
 			</div>
 		</div>
 		
 		<div class="row">
 			<div class="col-md-12">
-				<div class="page-header">
-					<h3>Vorrei aderire, ma sono da solo...</h3>
-				</div>
-		
-				<p>Se vorresti allestire anche tu il Linux Presentation Day nella tua città, ma hai bisogno di supporto, compila ugualmente il <a href="registrazione.php">form di registrazione</a>: spuntando l'apposita casella segnalerai il tuo interessamento, ed i nostri potenti algoritmi ci permetteranno di metterti in contatto con altri potenziali partecipanti nella tua zona per collaborare insieme!</p>
+				<p>
+					Vuoi attivarti anche tu per il Linux Presentation Day? Consulta <a href="faq.php">la pagina delle FAQ</a>!
+				</p>
 			</div>
 		</div>
 	</div>
 </div>
+
+<script>
+
+var map = L.map('map').setView([41.992, 11.755], 5);
+var marker = null;
+var display = false;
+
+L.tileLayer( "http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+	{
+		attribution: "Map data &copy; <a href=\"http://openstreetmap.org\">OpenStreetMap</a> contributors, <a href=\"http://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>"
+	}
+).addTo(map);
+
+map.on('click', function(e) {
+	var ll = e.latlng;
+
+	if (marker == null)
+		marker = L.marker(ll).addTo(map);
+	else
+		marker.setLatLng(ll);
+
+	$('input:hidden[name=lat]').val(ll.lat);
+	$('input:hidden[name=lng]').val(ll.lng);
+});
+
+<?php $file = file('data/true-registrations.csv'); foreach($file as $f): list($lat, $lng) = explode('|', $f); ?>
+L.marker([<?php echo $lat ?>, <?php echo $lng ?>]).addTo(map);
+<?php endforeach ?>
+
+</script>
 			
 <?php doFooter() ?>
 
